d285bd349270c816343ab8fef199d08acfa7d3ba
laiq6948 <lunaofscarlet@gmail.com>
2023-03-09 21:48:09 +0900

    commit on 2023.03.09

--- Algorithms/142. Linked List Cycle II.ts
+++ Algorithms/142. Linked List Cycle II.ts
@@ -11,17 +11,37 @@
  */
 
 function detectCycle(head: ListNode | null): ListNode | null {
-    if (!head || !head.next) {
-        return null;
-    }
-
-    let indicator: ListNode = head;
-    const nodes: ListNode[] = [];
-    while (indicator) {
-        if (nodes.includes(indicator)) return indicator;
-        nodes.push(indicator);
-        indicator = indicator.next;
-    }
-
+  if (!head || !head.next) {
     return null;
-};
+  }
+
+  let indicator: ListNode = head;
+  const nodes: ListNode[] = [];
+  while (indicator) {
+    if (nodes.includes(indicator)) return indicator;
+    nodes.push(indicator);
+    indicator = indicator.next;
+  }
+
+  return null;
+}
+
+// Two Pointer
+function detectCycle2(head: ListNode | null): ListNode | null {
+  let slow: ListNode | null = head,
+    fast: ListNode | null = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (fast === slow) {
+      let cur: ListNode = head;
+      while (cur !== slow) {
+        slow = slow.next;
+        cur = cur.next;
+      }
+      return cur;
+    }
+  }
+  return null;
+}
