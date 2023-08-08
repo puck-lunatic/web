9ceacb009266bf1c0334de23a6fa3948af5fd4db
laiq6948 <lunaofscarlet@gmail.com>
2023-08-09 00:48:50 +0900

    commit on 2023.08.08

--- Algorithms/33. Search in Rotated Sorted Array.rs
+++ Algorithms/33. Search in Rotated Sorted Array.rs
@@ -1,3 +1,4 @@
+// brute force
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         let mut i = 0;
@@ -8,5 +9,34 @@ impl Solution {
         
         if i == nums.len() { return -1; }
         i as i32
+    }
+}
+
+// binary search
+impl Solution2 {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+        while l <= r {
+            let mid = l + r >> 1;
+            if nums[mid] == target {
+                return mid as i32;
+            }
+
+            if nums[l] <= nums[mid] {
+                if target < nums[mid] && target >= nums[l] {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if target > nums[mid] && target <= nums[r] {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        -1
     }
 }
